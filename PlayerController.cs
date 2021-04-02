331111d20c2a3f81ac6c037e88dfcc9c0dde4ddf
Astagfiru <58816520+Astagfiru@users.noreply.github.com>
2021-04-02 17:02:58 +0900

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator anim;
+    float v;
+    float h;
+    float hv;
+    float lhv;
+    float brhv;
+    float blhv;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        v = Input.GetAxis("Vertical");
+        h = Input.GetAxis("Horizontal");
+        hv = h+v;
+        lhv = -(h-v);
+        brhv = -(v-h);
+        blhv = (h*v);
+        anim.SetFloat("runF", v);
+        anim.SetFloat("runR", h);
+        anim.SetFloat("runFS", hv);
+        anim.SetFloat("runFSL", lhv);
+        anim.SetFloat("runBS", brhv);
+        anim.SetFloat("runBSL", blhv);
+        if(Input.GetKeyDown("space"))
+        {
+            anim.SetTrigger("attack");
+        }
+    }
+
+    public void Poluchatel(float arg) 
+    {
+        transform.rotation = Quaternion.Euler(0,arg,0);
+    }
+}
---
+++ ZContr.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZContr : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform dest;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        agent.SetDestination(dest.position);
+    }
+
+    void OnCollisionEnter(Collision hit)
+    {
+        if(hit.gameObject.tag == "sword")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
